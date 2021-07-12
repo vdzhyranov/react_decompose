f5142035db44ef0f7cf64f72627b5864f678e764
vdzhyranov <v.dzhyranov@gmail.com>
2021-07-12 16:15:31 +0300

    Updates

---
+++ precache-manifest.c320d50c057835372a351357c6317789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178c19808d84d9fb8795dd67a078760d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a60324f768045f62bc6e",
+    "url": "./static/css/main.3cbf830c.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a60324f768045f62bc6e",
+    "url": "./static/js/main.a8f177cc.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
